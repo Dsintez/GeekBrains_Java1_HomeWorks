dcd7c5704dfad8bf04f24f6767235ce8a90c5e44
Dmirtry Sidorov <chaka.00@mail.ru>
2020-02-07 07:03:40 +0300

    Кошки едят еду

---
+++ src/HW7/Plate.java
@@ -0,0 +1,29 @@
+package HW7;
+
+public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void info(){
+        System.out.println("plate: " + food);
+    }
+
+    public void increaseFood(int piece){
+        if (piece < 0) return;
+        food = food + piece;
+    }
+
+    public boolean decreaseFood(int piece){
+        if (piece < 0) return false;
+        if (food - piece < 0) return false;
+        food = food - piece;
+        return true;
+    }
+
+    public int getFood() {
+        return food;
+    }
+}
